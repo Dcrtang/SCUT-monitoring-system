cdef133cea74c25aaf1c13aa2fdc344731dbd17c
MiloWang <milowang2048@foxmail.com>
2022-06-25 14:27:10 +0800

    file name conflicts

--- dashboard-front/src/main.tsx
+++ dashboard-front/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-import "./app.css";
+import "./App.css";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
